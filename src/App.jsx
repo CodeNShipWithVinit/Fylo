9bb724d3a80cc5917cbba256bc86da9cf671b929
Code-With-Vinit <122153636+Code-With-Vinit@users.noreply.github.com>
2026-03-15 02:20:08 +0530

    Implemented the Intro Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import logo from "../src/assets/logo.svg"
 import intro from "../src/assets/illustration-intro.png";
+import introBgMobile from "../src/assets/bg-curvy-mobile.svg";
 
 function App() {
   
@@ -22,7 +23,21 @@ function App() {
       </div>
 
       <section className='intro'>
-        <img src={intro} alt="intro" />
+        <img className="introImg" src={intro} alt="intro" />
+        <img className="introBgMobile" src={introBgMobile} alt="introBgMobile" />
+
+        <div className='introText'>
+          <h1><span>All your files in one secure</span><br />
+              <span>location,accesible</span><br />
+              <span>anywhere.</span>
+          </h1>
+          <p><span>Fylo stores all your most important files</span><br />
+             <span>in one secure location.Access them wherever</span><br />
+             <span>you need,share and collaborate with friends,</span><br />
+             <span>family and co-workers.</span>
+          </p>
+        </div>
+        
       </section>
      
     </div>
